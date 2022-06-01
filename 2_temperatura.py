416c6aa01edc0212e63f8c67e1da55ec617d10a5
João Victor de Oliveira Carneiro <97564969+ja1vctr@users.noreply.github.com>
2022-06-01 10:46:08 -0300

    Delete 2_temperatura.py

--- 2_temperatura.py
+++
@@ -1,6 +0,0 @@
-def funcoes():
-    print("Escreva um algoritmo que solicite ao usuário que informe uma temperatura em graus celcius\ne converta o valor para as escalas kelvin e farenheit.")
-
-
-if(__name__ == "__main__"):
-    funcoes()
